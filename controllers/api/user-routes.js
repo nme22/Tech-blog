24cab440e1c7cd3d5a113ef6a83a872757650969
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-04-16 00:30:39 -0400

    req.session.save work

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -16,8 +16,10 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       // TODO: SET USERID IN REQUEST SESSION TO ID RETURNED FROM DATABASE
+      req.session.userid = newUser.id;
 
       // TODO: SET USERNAME IN REQUEST SESSION TO USERNAME RETURNED FROM DATABASE
+      req.session.username = newUser.username;
 
       // TODO: SET LOGGEDIN TO TRUE IN REQUEST SESSION
 
